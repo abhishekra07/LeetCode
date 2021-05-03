7496db515f0d24478ac73bca10cc5a0ad5c9bef2
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2021-05-03 23:48:19 +0530

    Update AddTwoNumber.py

--- AddTwoNumber.py
+++ AddTwoNumber.py
@@ -19,6 +19,8 @@ class ListNode:
         self.val = val
         self.next = next
 
+
+# first solution came to mind
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         num1 = LinkedlistToInt(l1)
@@ -47,3 +49,53 @@ def LinkedlistToInt(head):
         position += 1
     return number
 
+
+
+#optimized one
+class Solution1:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        result = ListNode(0)
+        result_tail = result
+        carry = 0
+                
+        while l1 or l2 or carry:            
+            val1  = (l1.val if l1 else 0)
+            val2  = (l2.val if l2 else 0)
+            carry, out = divmod(val1+val2 + carry, 10)    
+                      
+            result_tail.next = ListNode(out)
+            result_tail = result_tail.next                      
+            
+            l1 = (l1.next if l1 else None)
+            l2 = (l2.next if l2 else None)
+               
+        return result.next
+
+
+"""
+Solution
+Approach 1: Elementary Math
+Intuition
+
+Keep track of the carry using a variable and simulate digits-by-digits sum starting from the head of list, which contains the least-significant digit.
+
+Algorithm
+
+Just like how you would sum two numbers on a piece of paper, we begin by summing the least-significant digits, which is the head of l1l1 and l2l2. Since each digit is in the range of 0 \ldots 90…9, summing two digits may "overflow". For example 5 + 7 = 125+7=12. In this case, we set the current digit to 22 and bring over the carry = 1carry=1 to the next iteration. carrycarry must be either 00 or 11 because the largest possible sum of two digits (including the carry) is 9 + 9 + 1 = 199+9+1=19.
+
+The pseudocode is as following:
+
+Initialize current node to dummy head of the returning list.
+Initialize carry to 0.
+Initialize pp and qq to head of l1l1 and l2l2 respectively.
+Loop through lists l1l1 and l2l2 until you reach both ends.
+Set xx to node pp's value. If pp has reached the end of l1l1, set to 0.
+Set yy to node qq's value. If qq has reached the end of l2l2, set to 0.
+Set sum = x + y + carrysum=x+y+carry.
+Update carry = sum / 10carry=sum/10.
+Create a new node with the digit value of (sum \bmod 10)(summod10) and set it to current node's next, then advance current node to next.
+Advance both pp and qq.
+Check if carry = 1carry=1, if so append a new node with digit 11 to the returning list.
+Return dummy head's next node.
+Note that we use a dummy head to simplify the code. Without a dummy head, you would have to write extra conditional statements to initialize the head's value.
+"""
